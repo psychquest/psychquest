e714f52411a083b84d19e4337606b43340ea4e5d
Tobias Israel <tobias.israel@posteo.de>
2018-07-11 09:22:05 +0200

    * update Neo4j settings to use embedded driver when running in dev * update SonarCloud settings

--- src/main/java/io/psychquest/app/infra/config/DatabaseConfiguration.java
+++ src/main/java/io/psychquest/app/infra/config/DatabaseConfiguration.java
@@ -1,12 +1,25 @@
 package io.psychquest.app.infra.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+
+import java.nio.file.Paths;
 
 @Configuration
 @EnableNeo4jRepositories(basePackages = "io.psychquest.app.infra.domain.model")
 @EntityScan(basePackages = "io.psychquest.app.infra.domain.model")
 public class DatabaseConfiguration {
 
+
+    @Profile("dev")
+    @Bean
+    public org.neo4j.ogm.config.Configuration configuration() {
+        String embeddedDBUri = Paths.get("target/neo4j.db").toAbsolutePath().toString();
+        return new org.neo4j.ogm.config.Configuration.Builder()
+            .uri("file://" + embeddedDBUri)
+            .build();
+    }
 }
